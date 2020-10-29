5fb31ad864446940052599c78200d1508effb457
jamiedishy <jamiedishy@gmail.com>
2020-10-29 15:54:11 -0400

    GridSearchCV - checks all permutations

--- part_II/android.py
+++ part_II/android.py
@@ -8,7 +8,7 @@ from sklearn.metrics import classification_report, f1_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 from pprint import pprint
 # import adtrees as adt
 
@@ -42,14 +42,14 @@ for classifier in classifier_array:
     print(classifier, '\n', classification_report(
         test_target, test_pred, labels=[0, 1]))
     if classifier == classifier_array[0]:
-        max_iter = [4, 5, 100, 120, 150]
+        max_iter = [4, 5, 100, 120, 1500]
         random_parameter_values = {'max_iter': max_iter}
         rf = LogisticRegression()
-        rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_parameter_values,
-                                       n_iter=2, cv=2, verbose=2, random_state=42, n_jobs=-1, scoring="f1_weighted")
+        rf_random = GridSearchCV(estimator=rf, param_grid=random_parameter_values,
+                                 cv=3, verbose=2, n_jobs=-1, scoring="f1_weighted")
         # Fit the random search model
         test_pred = rf_random.fit(train_data, train_target).predict(test_data)
-        print('\n Best Estimator: ', rf_random.best_estimator_)
+        # print('\n Best Estimator: ', rf_random.best_estimator_)
         print('\n LogisticRegression() - With Best Estimator Parameter Values')
         # pprint(rf_random.best_score_) MSU
         rf_best = rf_random.best_estimator_.fit(
@@ -61,8 +61,8 @@ for classifier in classifier_array:
         random_parameter_values = {'n_neighbors': n_neighbors,
                                    'leaf_size': leaf_size}
         rf = KNeighborsClassifier()
-        rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_parameter_values,
-                                       n_iter=4, cv=2, verbose=2, random_state=42, n_jobs=-1, scoring="f1_weighted")
+        rf_random = GridSearchCV(estimator=rf, param_grid=random_parameter_values,
+                                 cv=3, verbose=2, n_jobs=-1, scoring="f1_weighted")
         # Fit the random search model
         test_pred = rf_random.fit(train_data, train_target).predict(test_data)
         # print('\n Best Estimator: ', rf_random.best_estimator_)
@@ -72,22 +72,18 @@ for classifier in classifier_array:
             train_data, train_target).predict(test_data)
         print(classification_report(test_target, test_pred, labels=[0, 1]))
     elif classifier == classifier_array[2]:  # RandomForestClassifier()
-        n_estimators = [int(x)
-                        for x in np.linspace(start=200, stop=2000, num=10)]
-        max_features = ['auto', 'sqrt']
-        max_depth = [int(x) for x in np.linspace(10, 110, num=11)]
+        max_depth = [10, 2000, 400, 2]
         max_depth.append(None)
         bootstrap = [True, False]
-        max_leaf_nodes = [2, 3, 4, 6, 10]
+        max_leaf_nodes = [8, 4000, 600, 1200]
         max_leaf_nodes.append(None)
-        random_parameter_values = {'n_estimators': n_estimators,
-                                   'max_features': max_features,
-                                   'max_depth': max_depth,
-                                   'bootstrap': bootstrap,
-                                   'max_leaf_nodes': max_leaf_nodes}
+        random_parameter_values = {
+            'max_depth': max_depth,
+            'bootstrap': bootstrap,
+            'max_leaf_nodes': max_leaf_nodes}
         rf = RandomForestClassifier()
-        rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_parameter_values,
-                                       n_iter=4, cv=2, verbose=2, random_state=42, n_jobs=-1, scoring="f1_weighted")
+        rf_random = GridSearchCV(estimator=rf, param_grid=random_parameter_values,
+                                 cv=3, verbose=2, n_jobs=-1, scoring="f1_weighted")
         # Fit the random search model
         test_pred = rf_random.fit(train_data, train_target).predict(test_data)
         # print('\n Best Estimator: ', rf_random.best_estimator_)
