c19f311d0fcf84334714a802292f9340ddf876b2
jamiedishy <jamiedishy@gmail.com>
2020-10-31 23:41:27 -0400

    Part III - final testing and training data

--- part_III/android.py
+++ part_III/android.py
@@ -21,6 +21,7 @@ import heapq
 # step 1 - clean the data
 
 # Open data file and remove Id column
+print("Creating testing, training and comit dataframes")
 input_text_file = pd.read_csv(
     "../data/android/android-commit-messages.csv")
 input_training = pd.read_csv(
@@ -28,71 +29,64 @@ input_training = pd.read_csv(
 input_test = pd.read_csv(
     "../data/android/test.csv")
 
-comment = input_text_file.Comment
+comment_df = input_text_file.Comment
 id_column = input_text_file.Id
 
 # Remove special characters
+print("Removing special characters")
 spec_chars = ["!", '"', "#", "%", "&", "'", "(", ")",
               "*", "+", ",", "-", ".", "/", ":", ";", "<",
               "=", ">", "?", "@", "[", "\\", "]", "^", "_",
-              "`", "{", "|", "}", "~", "–", "nChange", "init", "Sig", " n n ",
-              " off by ", "\n", "$"]
-comment = comment.str.replace('|'.join(map(re.escape, spec_chars)), '')
-comment = comment.str.split().str.join(" ")
+              "`", "{", "|", "}", "~", "–", "$", "ø", "å"]
+comment_df = comment_df.str.replace('|'.join(map(re.escape, spec_chars)), '')
+comment_df = comment_df.str.split().str.join(" ")
 
 # Remove Ids
+print("Removing IDs")
 removed_id = []
-for row in comment:
+for row in comment_df:
     removed_id.append(
         re.sub(r'\w*\d\w*', '', row).strip()
     )
 
-# Stemming
+# Stemming, Remove stop words
+print("Tokenizing comments")
+print("Stemming, removing stop words and characters with a length of 1")
 w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
 stemmer = PorterStemmer()
+stop = stopwords.words('english')
 
 lemma_word = []
 place_holder = []
 for row in removed_id:
     place_holder = ""
     for word in w_tokenizer.tokenize(row):
-        place_holder += stemmer.stem(word) + " "
+        stem = stemmer.stem(word)
+        if stem.lower() not in stop and len(stem) > 2:
+            place_holder += stem + " "
     lemma_word.append(place_holder)
 
-
-# Remove stop words
-stop = stopwords.words('english')
-# comment = comment.apply(lambda x: [item for item in x if item not in stop])
-non_stop_words = []
-for row in lemma_word:
-    place_holder = ""
-    for word in w_tokenizer.tokenize(row):
-        if word.lower() not in stop:
-            place_holder += word + " "
-    non_stop_words.append(place_holder)
-
-df = pd.DataFrame(non_stop_words, columns=["Comment"])
-comment = df
-
-
-input_text_file = input_text_file.assign(Comment=comment)
+comment_df = pd.DataFrame(lemma_word, columns=["Comment"])
+print("View cleaned data in /part_III/data_cleaned/android/cleaned_data_android.csv")
+input_text_file = input_text_file.assign(Comment=comment_df)
 input_text_file.to_csv(
     './data_cleaned/android/cleaned_data_android.csv', index=False)
 
 
 # Bag of Words (1 - Gram)
-# Tokenize Comment section
-comment = input_text_file.Comment
+# Remove words with frequency <= 3
+print("Collecting most frequent words with frequency > 3")
+comment_df = input_text_file.Comment
 wordfreq = {}
 to_delete = list()
-for row in comment:
+for row in comment_df:
     for word in w_tokenizer.tokenize(row):
         if word not in wordfreq.keys():
             wordfreq[word] = 1
             to_delete.append(word)
         else:
             wordfreq[word] += 1
-            if (wordfreq[word] == 4):
+            if (wordfreq[word] == 40):
                 to_delete.remove(word)
 
 copy_wordfreq = dict(wordfreq)
@@ -101,10 +95,10 @@ for (key, value) in copy_wordfreq.items():
     if key in to_delete:
         del wordfreq[key]
 
-
+print("Creating table for most frequent words given comments")
 sentence_vectors = []
 count = 1
-for sentence in comment:
+for sentence in comment_df:
     sentece_tokens = nltk.word_tokenize(sentence)
     sent_vec = []
     # print(setence)
@@ -117,18 +111,20 @@ for sentence in comment:
 
 word_frequency_android_df = pd.DataFrame(sentence_vectors, columns=wordfreq)
 
+print("View most frequent word table in /part_III/data_cleaned/android/word_frequency_android.csv")
 word_frequency_android_df = word_frequency_android_df.assign(
     Id=input_text_file.Id)
 word_frequency_android_df.to_csv(
     './data_cleaned/android/word_frequency_android.csv', index=False)
 
-# # Sorted IDs for training data and test data
-# sorted_id_training_df = input_text_file.sort_values(by=["Id"])
-# # CSV of training comments
-# training_comment_android_df = sorted_id_training_df[sorted_id_training_df['Id'] <= 22328]
-# training_comment_android_df.to_csv(
-#     './data_cleaned/android/training_comment_data_android.csv', index=False)
-# # CSV of testing comments
-# testing_comment_android_df = sorted_id_training_df[sorted_id_training_df['Id'] > 22328]
-# testing_comment_android_df.to_csv(
-#     './data_cleaned/android/testing_comment_data_android.csv', index=False)
+print("View final training data with bag of words in /part_III/data_cleaned/android/training_data_final.csv")
+training_data_final_df = pd.merge(left=input_training, right=word_frequency_android_df,
+                                  how='left', left_on='Id', right_on='Id')
+training_data_final_df.to_csv(
+    './data_cleaned/android/training_data_final.csv', index=False)
+
+print("View final testing data with bag of words in part_III/data_cleaned/android/testing_data_final.csv")
+testing_data_final_df = pd.merge(left=input_test, right=word_frequency_android_df,
+                                 how='left', left_on='Id', right_on='Id')
+testing_data_final_df.to_csv(
+    './data_cleaned/android/testing_data_final.csv', index=False)
